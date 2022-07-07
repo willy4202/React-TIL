937c0aa072355c5dece9f343a7871c4e9a4eb7f8
Young <willy4202@naver.com>
2022-07-07 18:42:17 +0900

    add: 무한스크롤 레이아웃 구현

--- src/InfiniteScroll/InfiniteScroll.js
+++ src/InfiniteScroll/InfiniteScroll.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
+const API_KEY = "8f315c9787c0d2e3bdb126d07e5b94ac";
+const GET_IMAGE = "http://image.tmdb.org/t/p/w500";
+
 const InfiniteScroll = () => {
-  fetch(
-    " http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=0460926fdea8bad48e2a5e8c8baf33a0&itemPerPage=100"
-  )
-    .then((res) => res.json())
-    .then((item) => console.log(item));
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=ko-KR&page=1`
+    )
+      .then((res) => res.json())
+      .then((item) => setMovies(item.results));
+  }, []);
+
+  console.log(movies);
 
   return (
     <Layout>
@@ -14,7 +23,7 @@ const InfiniteScroll = () => {
         <h1>welcome InfiniteScroll</h1>
       </header>
 
-      <article>
+      <aside>
         <h2>무한 스크롤의 장점</h2>
         무한 스크롤은 사용자가 페이지 하단에 도달했을 때, 콘텐츠가 계속 로드되는
         사용자 경험(UX) 방식입니다. 한 페이지 아래로 스크롤 하면 끝없이 새로운
@@ -44,15 +53,28 @@ const InfiniteScroll = () => {
           <li>스크롤 막대가 실제 데이터양을 반영하지 못합니다. </li>
           <li>푸터를 찾기 어려워집니다.</li>
         </ul>
-      </article>
-      <main>
+      </aside>
+      <aside>
         <h2>기술 선택 이유</h2>
         <h3>- IntersectionObserver API</h3>
         <div>
           인터섹션 api는 디바운스, 쓰로틀을 사용하지 않아도 되고, reflow현상이
           발견되지 않기 때문에 해당 api를 사용해서 구현합니다.
         </div>
-      </main>
+      </aside>
+      <h1>InfiniteScroll Example</h1>
+      <MovieLayout>
+        {movies.length !== 0 &&
+          movies.map(({ id, poster_path, original_title, overview }) => {
+            return (
+              <Post key={id}>
+                <Image src={`${GET_IMAGE}${poster_path}`} alt="영화포스터" />
+                <h4>{original_title}</h4>
+                <Overview>{overview}</Overview>
+              </Post>
+            );
+          })}
+      </MovieLayout>
     </Layout>
   );
 };
@@ -65,4 +87,25 @@ const Layout = styled.div`
   align-items: center;
   max-width: 800px;
   margin: 0 auto;
+`;
+
+const MovieLayout = styled.main`
+  display: grid;
+  grid-template-columns: repeat(2, 300px);
+  grid-gap: 30px;
+`;
+
+const Post = styled.article`
+  width: 300px;
+  margin: 0 auto;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 420px;
+`;
+
+const Overview = styled.div`
+  font-size: 14px;
+  color: gray;
 `;
