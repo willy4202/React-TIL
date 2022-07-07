5de9d2646105084596714feb48d6eee01b2740a2
Young <willy4202@naver.com>
2022-07-07 14:02:35 +0900

    add: 무한스크롤 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import InfiniteScroll from "./InfiniteScroll/InfiniteScroll";
 import Pagenation from "./Pagenation/Pagenation";
 import Redux from "./Redux";
 
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Redux />} />
         <Route path="/1" element={<Pagenation />} />
+        <Route path="/2" element={<InfiniteScroll />} />
       </Routes>
     </>
   );
