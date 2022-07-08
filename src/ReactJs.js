5028f22e448a0a3e8f685fbcb4196da0fbcf2332
Young <willy4202@naver.com>
2022-07-08 21:09:24 +0900

    add: redux, redux-devtools, logger 설치

--- src/ReactJs.js
+++ src/ReactJs.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
 const ReactJs = () => {
-  return <div>Welcome ReactJs</div>;
+  return <Layout>Welcome ReactJs</Layout>;
 };
 
 export default ReactJs;
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 800px;
+  margin: 0 auto;
+`;
